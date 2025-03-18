6d4e0080957073918febdb99b2930d1eb51c6af9
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-18 15:23:21 +0100

    Add FizzBuzz output for multiples of 3 and 5

--- js/script.js
+++ js/script.js
@@ -5,4 +5,10 @@ for (let i=1; i<=100; i=i+1){
     if (i%3 == 0 && i%5 == 0){
         console.log(fizz + buzz);
     }
+    else if (i%3 == 0){
+        console.log(fizz);
+    }
+    else if (i%5 == 0){
+        console.log(buzz);
+    }
 }
